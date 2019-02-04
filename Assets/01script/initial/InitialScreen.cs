98bfc153e6bd03fec2b317e272499a166422fe2c
watanabe.everystudio <watanabe.everystudio@gmail.com>
2019-02-04 17:57:01 +0900

    起動時の画面サイズ制御 ほんとのしょっぱなは制御できないっぽい

---
+++ Assets/01script/initial/InitialScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialScreen //: MonoBehaviour 
+{
+	[RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad()
+    {
+        Screen.SetResolution( 1280, 720, false, 60);
+    }
+}
